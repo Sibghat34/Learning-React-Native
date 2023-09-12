328f11487c3dfafb6774c0ea71f4d3ebebbba77d
Sibghat Ullah <sibghat.aamir34@gmail.com>
2023-09-13 01:50:54 +0500

    style inputFeild with flex

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View>
-    <View>
-      <TextInput placeholder="Add your dream car" />
+    <View style = {styles.appContainer}>
+    <View style = {styles.inputContainer}>
+      <TextInput style = {styles.inputText} placeholder="Add your dream car" />
       <Button title="ADD" />
     </View>
     <View>
@@ -16,5 +16,17 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
- 
+ appContainer: {
+  padding: 50
+ },
+ inputContainer: {
+  flexDirection: 'row',
+  justifyContent: 'space-between'
+ },
+ inputText: {
+  borderWidth: 1,
+  borderRadius: 5,
+  width: '80%',
+  paddingLeft: 10
+ }
 });
