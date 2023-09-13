0703dbb882ee3e55ef6b4614843e324897ef75a7
Sibghat Ullah <sibghat.aamir34@gmail.com>
2023-09-13 05:41:01 +0500

    items added in the list

--- App.js
+++ App.js
@@ -1,42 +1,56 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [carsList, setCarsList] = useState([]);
+
+  function carInputHandler(input) {
+    setInput(input);
+  }
+  function addCarHandler() {
+    setCarsList((currentCarsList) => [...currentCarsList, input]);
+  }
+
   return (
-    <View style = {styles.appContainer}>
-    <View style = {styles.inputContainer}>
-      <TextInput style = {styles.inputText} placeholder="Add your dream car" />
-      <Button title="ADD" />
-    </View>
-    <View style = {styles.listContainer}>
-      <Text>List of Cars..........!</Text>
-    </View>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Add your dream car"
+          onChangeText={carInputHandler}
+        />
+        <Button title="ADD" onPress={addCarHandler} />
+      </View>
+      <View style={styles.listContainer}>
+        {carsList.map((car) => <Text key={car}>{car}</Text>)}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
- appContainer: {
-  flex: 1,
-  paddingTop: 70,
-  paddingHorizontal: 20
- },
- inputContainer: {
-  flex: 1,
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  marginBottom: 20,
-  borderBottomWidth: 1,
-
- },
- inputText: {
-  borderWidth: 1,
-  borderRadius: 5,
-  width: '80%',
-  paddingLeft: 10
- },
- listContainer: {
-  flex: 4,
- }
+  appContainer: {
+    flex: 1,
+    paddingTop: 70,
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    borderBottomWidth: 1,
+  },
+  inputText: {
+    borderWidth: 1,
+    borderRadius: 5,
+    width: "80%",
+    paddingLeft: 10,
+  },
+  listContainer: {
+    flex: 4,
+  },
 });
