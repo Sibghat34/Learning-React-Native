dbe0a050e7f91c1848f3fcea3e447123f98d44aa
Sibghat Ullah <sibghat.aamir34@gmail.com>
2023-09-13 05:10:09 +0500

    layout update

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
       <TextInput style = {styles.inputText} placeholder="Add your dream car" />
       <Button title="ADD" />
     </View>
-    <View>
+    <View style = {styles.listContainer}>
       <Text>List of Cars..........!</Text>
     </View>
     </View>
@@ -17,16 +17,26 @@ export default function App() {
 
 const styles = StyleSheet.create({
  appContainer: {
-  padding: 50
+  flex: 1,
+  paddingTop: 70,
+  paddingHorizontal: 20
  },
  inputContainer: {
+  flex: 1,
   flexDirection: 'row',
-  justifyContent: 'space-between'
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginBottom: 20,
+  borderBottomWidth: 1,
+
  },
  inputText: {
   borderWidth: 1,
   borderRadius: 5,
   width: '80%',
   paddingLeft: 10
+ },
+ listContainer: {
+  flex: 4,
  }
 });
