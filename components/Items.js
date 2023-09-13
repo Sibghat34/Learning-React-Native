825a044d429bab05bea12684e8ac0fdec79f71fc
Sibghat Ullah <sibghat.aamir34@gmail.com>
2023-09-14 02:43:07 +0500

    visual effect add on delete item

--- components/Items.js
+++ components/Items.js
@@ -2,21 +2,32 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function Items(props) {
   return (
-    <Pressable onPress={props.onDeleteCar.bind(this, props.id)}>
-    <View style={styles.listItems}>
-      <Text>{props.text}</Text>
-    </View>
-    </Pressable>
+   
+      <View style={styles.listItems}>
+         <Pressable
+    //   android_ripple={{ color: "darkgry" }}
+      onPress={props.onDeleteCar.bind(this, props.id)}
+      style = {({pressed}) => pressed && styles.itemPressed}
+    >
+        <Text style = {styles.itemText}>{props.text}</Text>
+        </Pressable>
+      </View>
+    
   );
 }
 
 export default Items;
 
 const styles = StyleSheet.create({
-    listItems: {
-        margin: 10,
-        padding: 10,
-        backgroundColor: "lightgrey",
-        borderRadius: 10,
-      },
+  listItems: {
+    margin: 10,
+    backgroundColor: "lightgrey",
+    borderRadius: 10,
+  },
+  itemText: {
+    padding: 10,
+  },
+  itemPressed: {
+    opacity: 0.2
+  }
 });
